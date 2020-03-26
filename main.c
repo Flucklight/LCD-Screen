e23322fb974234880235403825b94045775fdc34
Julio César Bolio <42160567+jc-bolio@users.noreply.github.com>
2020-03-25 22:21:04 -0600

    Cambios a logica positiva

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 #include<lcd.c>                                                //Incluimos la libreria para la pantalla LCD
 
-int8 efect;                                              //Variable para la funcion de movimiento
+int8 a;                                              //Variable para la funcion de movimiento
 void main() {
    
    lcd_init();                                               //Instruccion para la funcion de inicializacion de la pantalla LCD
@@ -13,20 +13,20 @@ void main() {
 
       printf(lcd_putc, "\f");                                //Limpiamos pantalla
 
-      for(efect = 16; efect >= 0; efect = efect - 1) {       //Este for genera un efecto de recorrido en la pantalla
-         lcd_gotoxy(efect,1);                                //Mueve el apuntador interno a la 1ra fila y decrementa conforme a la variable en la columna
+      for(a = 0; a <= 10; a++) {       //Este for genera un efecto de recorrido en la pantalla
+         lcd_gotoxy(a,1);                                //Mueve el apuntador interno a la 1ra fila y decrementa conforme a la variable en la columna
          printf(lcd_putc, "Julio Cesar");                    //Manda a imprimir el nombre "Julio Cesar"
-         lcd_gotoxy(efect,2);                                //Mueve el apuntador interno a la 2da fila y decrementa conforme a la variable en la columna
+         lcd_gotoxy(a,2);                                //Mueve el apuntador interno a la 2da fila y decrementa conforme a la variable en la columna
          printf(lcd_putc, "Jimenez Bolio");                  //Manda a imprimir el apellido "Jimenez Bolio"
          delay_ms(100);                                      //Tiene un retraso de 100 milisegundos
       }
 
       printf(lcd_putc, "\f");                               //Limpiamos pamtalla
 
-      for(efect = 16; efect >= 0; efect = efect - 1) {      //Este for genera un efecto de recorrido en la pantalla
-         lcd_gotoxy(efect,1);                               //Mueve el apuntador interno a la 1ra fila y decrementa conforme a la variable en la columna
+      for(a = 0; a <= 10; a++) {      //Este for genera un efecto de recorrido en la pantalla
+         lcd_gotoxy(a,1);                               //Mueve el apuntador interno a la 1ra fila y decrementa conforme a la variable en la columna
          printf(lcd_putc, "Victor Manuel");                 //Manda a imprimir el nombre "Victor Manuel"
-         lcd_gotoxy(efect,2);                               //Mueve el apuntador interno a la 2da fila y decrementa conforme a la variable en la columna
+         lcd_gotoxy(a,2);                               //Mueve el apuntador interno a la 2da fila y decrementa conforme a la variable en la columna
          printf(lcd_putc, "Sanchez Sanchez");               //Manda a imprimir el apellido "Sanchez Sanchez"
          delay_ms(100);                                     //Tiene un retraso de 100 milisegundos
       }
